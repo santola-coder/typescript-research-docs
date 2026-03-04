f3f52c2d5aeaee6cfe6a7edc02cb5303ac9c978d
santola.developer@gmail.com <santola.developer@gmail.com>
2026-03-04 09:02:55 +0700

    learning typescript from beginner to interme

---
+++ typescript-basic/src/index.ts
@@ -0,0 +1,10 @@
+// 01-basics/variables.tsx
+
+let number: number[] = [20, 30, 40, 50];
+let users: string[] = ["San tola", "O tani"];
+
+let sumNumbers = number.map(n =>n+)
+
+// Example 2: Make usernames uppercase
+let upperUsers = users.map(name => name.toLowerCase());
+console.log(`${upperUsers}`);
