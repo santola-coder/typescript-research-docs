d425dfb304b5ede107f219c8ef2656b13ad27577
santola.developer@gmail.com <santola.developer@gmail.com>
2026-03-18 08:44:50 +0700

    add file readme more contains ts quiz

---
+++ typescript-learnig/src/index.ts
@@ -0,0 +1,12 @@
+let nums: number[] = [1, 2, 3];
+let tags: Array<string> = ["ts", "js"];
+
+let user: { name: string; age: number } = {
+  name: "ToLa",
+  age: 21,
+};
+
+
+const [first, second, third] = nums; 
+
+console.log(nums);
