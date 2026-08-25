7b3c9cb08f948ab57bb8d23909598c982a05fc7c
Tola San <santola.developer@gmail.com>
2026-08-25 19:44:35 +0700

    learn ts

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-## 📘 TypeScript Learning Notes
+## 📘 TypeScript Learning Notes  
 
 --
 
--- typescript-learnig/src/index.ts
+++ typescript-learnig/src/index.ts
@@ -1,12 +1,69 @@
-let nums: number[] = [1, 2, 3];
-let tags: Array<string> = ["ts", "js"];
-
-let user: { name: string; age: number } = {
-  name: "ToLa",
-  age: 21,
-};
 
 
-const [first, second, third] = nums; 
+/*
+Tpypescript version used in this project is : 6.0.0-dev.20260416
+The TypeScript module system used in this project is : NodeNext
+The TypeScript module resolution strategy used in this project is : NodeNext
+The target version of JavaScript for this project is : ES2022
+Library files that should be included in TypeScript compilation are : lib.es2022.d.ts
 
-console.log(nums);
+*/
+// ------------------------------------------------------------------------------------
+
+// javascript code
+
+// let name = "San tola";
+// let age = 23;
+// let email = "stola@hengdebug.edu";
+// let telphone = "012555358";
+
+// typescript code
+
+let name: string = "San tola";
+let age: number = 23;
+let email: string = "stola@hengdebug.edu";
+let telphone = "012555358";
+console.log(name,age,telphone);
+
+console.log("-----------------------------------------------------------------------------");
+
+// /defien array of objecg typescript 
+
+const skill: string[] = ["javascript", "typescrip","ASP.NET WEB API", "java spring boot", "laravel"];
+console.log(skill);
+
+console.log("-----------------------------------------------------------------------------");
+
+
+
+//  deifine object and create object 
+
+interface Student  {
+    id: number,
+    name: string,
+    email:string,
+    phone?:string | null,
+}
+
+const student: Student = {
+
+    id: 10,
+    name: "san tola",
+    email:"stola@example.com",
+    phone: null
+}
+
+
+
+//  create method for getProperty obejct student 
+
+function GetProp(student: Student): void {
+
+console.log(student.id);
+console.log(student.name);
+console.log(student.email);
+console.log(student.phone);
+}
+
+GetProp(student);
+
